2f490d6b36a29ab3ea46557ec638fe0f4888fec2
Xzapher <c4rlos275@gmail.com>
2025-04-22 01:31:17 +0200

    Add project files.

---
+++ ReadJson/ProductosModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ReadJson.Models
+{
+    internal class ProductoModel
+    {
+        //[Key]
+        //[Column("ID")]
+        [JsonProperty("ID")]
+        public int Id { get; set; }
+
+        //[Column("Nombre")]
+        [JsonProperty("Nombre")]
+        public string Nombre { get; set; }
+
+        //[Column("Precio")]
+        [JsonProperty("PrecioBase")]
+        public decimal PrecioBase { get; set; }
+
+        //[Column("Categoria")]
+        [JsonProperty("Categoria")]
+        public string Categoria { get; set; }
+    }
+}
---
+++ ReadJson/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using ReadJson.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string productosJsonFile = "../../../Productos.json";
+
+        string productosJsonContent = File.ReadAllText(productosJsonFile);
+
+        //if (File.Exists(jsonFilePath))
+        //{
+        //    string jsonContent = File.ReadAllText(jsonFilePath);
+        //    Console.WriteLine("JSON content:");
+        //    Console.WriteLine(jsonContent);
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"File not found: {jsonFilePath}");
+        //}
+        //Console.WriteLine("Press any key to exit...");
+        //Console.ReadKey();
+
+        List<ProductoModel> productosJson = JsonConvert.DeserializeObject<List<ProductoModel>>(productosJsonContent);
+
+        var productoId4 = productosJson.SingleOrDefault(e => e.Id == 4);
+
+        Console.WriteLine($"Id: {productoId4.Id}, Nombre: {productoId4.Nombre}, Precio: {productoId4.PrecioBase}, Categoria: {productoId4.Categoria}");
+
+        //var query1 = productosJson.Where(e => e.Precio <= 4.95);
+
+        //foreach (var producto in query1)
+        //{
+        //    Console.WriteLine($"Id: {producto.Id}, Nombre: {producto.Nombre}, Precio: {producto.Precio}, Categoria: {producto.Categoria}");
+        //}
+
+        //var filteredProducts = productosJson.Find(e => e.Id == 1);
+
+        //foreach (var producto in filteredProducts)
+        //{
+        //    Console.WriteLine($"Id: {producto.Id}, Nombre: {producto.Nombre}, Precio: {producto.Precio}, Categoria: {producto.Categoria}");
+        //}
+
+        Console.ReadKey();
+    }
+}
